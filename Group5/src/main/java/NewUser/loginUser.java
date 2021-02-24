9e183aefc0d5dcb8db22c6fd4fddc6ec231353be
Dharaben Thakorbhai Gohil <dh447205@dal.ca>
2021-02-24 20:26:06 +0530

    deleted unwanted data

--- Group5/src/main/java/NewUser/loginUser.java
+++
@@ -1,9 +0,0 @@
-package NewUser;
-
-public class loginUser {
-	
-	public static void varifyUser(int cuId, int password) {
-		
-	}
-
-}
--- Group5/src/main/java/TrainBooking/Train.java
+++
@@ -1,4 +0,0 @@
-package TrainBooking;
-
-public class Train {
-}
